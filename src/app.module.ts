7282db22c043593a0f50430d2122b5fb92615774
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-10-08 17:35:03 -0400

    update seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 @Module({
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'public'),
-  }), MongooseModule.forRoot('mongodb://localhost/pokeApi'), PokemonModule, CommonModule],
+  }), MongooseModule.forRoot('mongodb://localhost/pokeApi'), PokemonModule, CommonModule, SeedModule],
   controllers: [],
   providers: [],
 })
