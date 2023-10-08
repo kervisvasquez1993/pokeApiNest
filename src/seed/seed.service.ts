53ccc7c570ce11121a74bb99d3ea08c863eb06ff
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-10-08 18:07:20 -0400

    call dependency in pokeApi

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return "seed Executed successfully"
+  private readonly axios: AxiosInstance = axios
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=10")
+    data.results.forEach(({name, url}) => {
+      const segment = url.split("/")
+      const no:number = +segment[segment.length - 2]
+      console.log(name,url, no);
+    })
+    
+    return data.results
   }
 }
